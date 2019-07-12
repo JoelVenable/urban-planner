1c2a33c279880f9e88af65f6805383cdafe655d8
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-12 11:14:59 -0500

    Added Building class

---
+++ Building.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Planner
+{
+  public class Building
+  {
+    private string _designer;
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get
+      {
+        return Width * Depth * (3 * Stories);
+      }
+    }
+
+    public Building(string address)
+    {
+      _address = address;
+      _designer = "Joel Venable";
+    }
+
+    public void Construct()
+    {
+      _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase(string purchaser)
+    {
+      _owner = purchaser;
+    }
+  }
+}
